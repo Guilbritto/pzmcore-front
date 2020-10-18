7788845927d1dc154b2c3055c83ef2512ed27cdf
Guilherme Brito <guilhermebritto.prof@gmail.com>
2020-10-18 18:18:03 -0300

    Fix: Remove de slash to de url file

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,9 +1,8 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
 //Login
-export const LOGIN = `${BASE_URL}/sessions`;
-export const FORGOT_PASSWORD = `${BASE_URL}/sessions/forgot`
-export const FORGOT_PASSWORD_VALIDATE = `${BASE_URL}/sessions/forgot/validate`
-export const FORGOT_PASSWORD_CHANGE = `${BASE_URL}/sessions/forgot/change`
+export const LOGIN = `sessions`;
+export const FORGOT_PASSWORD = `sessions/forgot`
+export const FORGOT_PASSWORD_VALIDATE = `sessions/forgot/validate`
+export const FORGOT_PASSWORD_CHANGE = `sessions/forgot/change`
 
 //SIGNUP
-export const SIGNUP = `${BASE_URL}/user`;
+export const SIGNUP = `/user`;
