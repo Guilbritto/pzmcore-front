fc24f4666dca47d46283c7190bede1afca624b8e
Guilherme Brito <guilhermebritto.prof@gmail.com>
2020-10-18 18:04:21 -0300

    Fix: Change the base url from urls file

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3333';
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 //Login
 export const LOGIN = `${BASE_URL}/sessions`;
 export const FORGOT_PASSWORD = `${BASE_URL}/sessions/forgot`
